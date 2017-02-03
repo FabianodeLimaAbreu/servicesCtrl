0efb0743594f255507c62b15d7ff11306e673e45
Fabiano de Lima Abreu <fabianodelimaabreu@gmail.com>
2017-02-03 09:44:45 -0200

    Working in ambient struture
    Config load plugin
    Starting MVC Pattern

--- github/index.js
+++ github/index.js
@@ -1,24 +1,33 @@
 //require("./models/usuarios")
 
-var express=require("express");
-var app=express();
-var url = require('url');
-var request = require("request");
-var routes=require("./routes");
+var express=require("express")
+	,url = require('url')
+	,request = require("request")
+	,load=require("express-load")
+	,app=express()
+	,mongoose=require("mongoose")
+	,bodyParser = require('body-parser')
+	,methodOverride = require('method-override')
+	,cors=require("cors")
+;
+
+//global.db=mongoose.connect("mongodb://localhost/app");
+
 //var mongoose=require("mongoose");
 // mongoose.connect("mongodb://189.126.197.169/app");
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+app.use(cors());
 
-app.use(app.router);
-app.get("/node/servicesctrl/teste",routes.teste);
+load("models")
+	.then("controllers")
+	.then("routes")
+	.into(app);
 
-/*app.get('/node/app/teste', function(req,res,next){
- 
-  res.writeHead(200, {
-    'Content-Type': 'application/json; charset=utf-8',
-    'Access-Control-Allow-Credentials': true,
-    'Access-Control-Allow-Origin': '*'
-  });
+//app.get("/node/servicesctrl_dev/teste",qrapp.teste);
 
-   res.end('Cheguei');
-});*/
-app.listen(process.env.PORT);
+app.listen(process.env.PORT);	
